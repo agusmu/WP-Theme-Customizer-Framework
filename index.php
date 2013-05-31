4820f681cee6acf29f9a923f2d9be0d31485cce1
Chris Kelley <chris@organicbeemedia.com>
2013-05-31 13:36:43 -0500

    cw_get_option changed to cw_theme_option

--- index.php
+++ index.php
@@ -5,21 +5,21 @@
 
 <ul>
 	
-	<li><?php if(cw_get_option('cw_text')){ echo cw_get_option('cw_text'); } ?></li>
+	<li><?php if( cw_theme_option('cw_text')){ echo cw_theme_option('cw_text', 'test'); } ?></li>
 
-	<li><?php if(cw_get_option('cw_textarea')){ echo cw_get_option('cw_textarea'); } ?></li>
+	<li><?php if(cw_theme_option('cw_textarea')){ echo cw_theme_option('cw_textarea'); } ?></li>
 
-	<li><?php if(cw_get_option('cw_checkbox')){ echo cw_get_option('cw_checkbox'); } ?></li>
+	<li><?php if(cw_theme_option('cw_checkbox')){ echo cw_theme_option('cw_checkbox'); } ?></li>
 	
-	<li><?php if(cw_get_option('cw_radio')){ echo cw_get_option('cw_radio'); } ?></li>
+	<li><?php if(cw_theme_option('cw_radio')){ echo cw_theme_option('cw_radio'); } ?></li>
 	
-	<li><?php if(cw_get_option('cw_color')){ echo cw_get_option('cw_color'); } ?></li>
+	<li><?php if(cw_theme_option('cw_color')){ echo cw_theme_option('cw_color'); } ?></li>
 	
-	<li><?php if(cw_get_option('cw_image')){ echo cw_get_option('cw_image'); } ?></li>
+	<li><?php if(cw_theme_option('cw_image')){ echo cw_theme_option('cw_image'); } ?></li>
 	
-	<li><?php if(cw_get_option('cw_file')){ echo cw_get_option('cw_file'); } ?></li>
+	<li><?php if(cw_theme_option('cw_file')){ echo cw_theme_option('cw_file'); } ?></li>
 
-	<li><?php if(cw_get_option('cw_pages')){ echo cw_get_option('cw_pages'); } ?></li>
+	<li><?php if(cw_theme_option('cw_pages')){ echo cw_theme_option('cw_pages'); } ?></li>
 
 </ul>
 
